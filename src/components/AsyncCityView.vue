0c483c4f90733312c0d1eccb9d67416c15593ba3
Arnie <arnie752@gmail.com>
2024-02-06 21:56:47 +0300

    bugfix/AsyncCityView: Error 401 Fixed in regards to the OpenWeather API Call
    The API Call now returns the current weather data.
    A new API call wil have to be made that gets the 5 day forecast data

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
+  <div class="flex flex-col flex-1 items-center px-6">
+    <!-- Banner -->
+    <div v-if="route.query.preview" class="text-white p-4 container text-center rounded-3xl">
+      <p>You are currently previewing the city, click the "+" icon to start tracking this city</p>
+    </div>
+    <!-- Weather Overview -->
+    <div>
 
+    </div>
   </div>
 </template>
 
@@ -13,20 +20,20 @@ const openWeatherAPIKey = 'ff44687c4aab2884ff50d39ba2bb14a7'
 const route = useRoute()
 const getWeatherData = async () => {
   try {
-    const weatherData = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=${openWeatherAPIKey}&units=metric`)
-
+    const weatherData = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${route.query.lat}&lon=${route.query.lng}&appid=${openWeatherAPIKey}&units=metric`)
+    // console.log(weatherData)
     // cal current date & time
     const localOffset = new Date().getTimezoneOffset() * 60000;
-    const utc = weatherData.data.current.dt * 1000 + localOffset;
+    const utc = weatherData.data.dt * 1000 + localOffset;
     weatherData.data.currentTime =
       utc + 1000 * weatherData.data.timezone_offset;
 
-    // cal hourly weather offset
-    weatherData.data.hourly.forEach((hour) => {
-      const utc = hour.dt * 1000 + localOffset;
-      hour.currentTime =
-        utc + 1000 * weatherData.data.timezone_offset;
-    });
+    // // cal hourly weather offset
+    // weatherData.data.hourly.forEach((hour) => {
+    //   const utc = hour.dt * 1000 + localOffset;
+    //   hour.currentTime =
+    //     utc + 1000 * weatherData.data.timezone_offset;
+    // });
 
     return weatherData.data;
   } catch (error) {
